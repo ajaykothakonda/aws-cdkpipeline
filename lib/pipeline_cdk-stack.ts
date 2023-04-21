1496f4d95a604196a182e9b5e9a4600786546065
Pierre <pierre_kengne@yahoo.com>
2023-04-21 15:43:34 -0400

    reinit

--- lib/pipeline_cdk-stack.ts
+++ lib/pipeline_cdk-stack.ts
@@ -138,7 +138,7 @@ export class PipelineCdkStack extends cdk.Stack {
       adminPermissions: true
     }))
   };
-  /*
+  
   public addServiceIntegrationTestStage(stage:IStage, serviceEndpoint: string) {
     stage.addAction(
       new CodeBuildAction({
@@ -161,5 +161,5 @@ export class PipelineCdkStack extends cdk.Stack {
       })
     )    
   }
-  */
+  
 }
